7f2518477342c5adaa5e70f25eca140500831e94
Sai Krishna Resu <143726106+saikrishnaresuk@users.noreply.github.com>
2026-08-21 17:47:59 +0530

    commit

--- Day-7/main.tf
+++ Day-7/main.tf
@@ -3,30 +3,30 @@ provider "aws" {
 }
 
 provider "vault" {
-  address = "<>:8200"
+  address = "http://100.48.211.139:8200"
   skip_child_token = true
 
   auth_login {
     path = "auth/approle/login"
 
     parameters = {
-      role_id = "<>"
-      secret_id = "<>"
+      role_id = "cbb2952f-bbd0-ac4e-f98c-62e19d047419"
+      secret_id = "64c4eb4c-8677-5a79-f2ad-db8d74a7c041"
     }
   }
 }
 
 data "vault_kv_secret_v2" "example" {
-  mount = "secret" // change it according to your mount
+  mount = "kv" // change it according to your mount
   name  = "test-secret" // change it according to your secret
 }
 
 resource "aws_instance" "my_instance" {
-  ami           = "ami-053b0d53c279acc90"
+  ami           = "ami-0b6d9d3d33ba97d99"
   instance_type = "t2.micro"
 
   tags = {
     Name = "test"
-    Secret = data.vault_kv_secret_v2.example.data["foo"]
+    Secret = data.vault_kv_secret_v2.example.data["username"]
   }
 }
